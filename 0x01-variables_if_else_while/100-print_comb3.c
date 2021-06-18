52c5229558cd00e650a9a54aa2d7bc34a4ba15ad
Da-de-hash <ovedcheg@gmail.com>
2021-06-18 14:24:08 -0700

    print comb

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,11 +8,11 @@
  * Description: Writes all unique combinations
  * of 2 numbers
  *
- * Return 0 for success
+ * Return: 0 for success
 */
 int main(void)
 {
-int i,j;
+int i, j;
 for (i = '0'; i <= '9'; i++)
 {
 for (j = '0'; j <= '9'; j++)
