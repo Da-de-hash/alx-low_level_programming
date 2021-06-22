75088a0eab4a3c2b3f77b9d10eed615f84a1c8db
Da-de-hash <ovedcheg@gmail.com>
2021-06-22 12:13:01 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ *_abs - computers absolute value
+ *
+ *@n: int to compute as absolute
+ *
+ * Return: absolute value
+ **/
+int _abs(int n)
+{
+if (n < 0)
+{
+return (-n);
+}
+else
+{
+return (n);
+}
+}
